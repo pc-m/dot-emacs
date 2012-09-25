b041ae810f09c1fc0d30a158d7f92d7cb86e41d4
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-09-25 07:33:29 -0400

    Add advice to place the closing [)]}] at the good place when editing python

--- init.el
+++ init.el
@@ -120,7 +120,24 @@
 		 (delq nil (delete-dups
 			    (append
 			     (get-global-pythonpath)
-			     (get-project-pythonpath)))) ":"))))
+			     (get-project-pythonpath)))) ":"))
+    (defadvice python-calculate-indentation (around outdent-closing-brackets)
+      "Handle lines beginning with a closing bracket and indent them so that
+       they line up with the line containing the corresponding opening bracket."
+      (save-excursion
+	(beginning-of-line)
+	(let ((syntax (syntax-ppss)))
+	  (if (and (not (eq 'string (syntax-ppss-context syntax)))
+		   (python-continuation-line-p)
+		   (cadr syntax)
+		   (skip-syntax-forward "-")
+		   (looking-at "\\s)"))
+	      (progn
+		(forward-char 1)
+		(ignore-errors (backward-sexp))
+		(setq ad-return-value (current-indentation)))
+	    ad-do-it))))
+    (ad-activate 'python-calculate-indentation)))
 
 (use-package pymacs
   :init
