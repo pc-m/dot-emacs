e38a0d0ac7d3b28ba971ba57f6cde25179d12ce2
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2013-04-22 08:45:56 -0400

    Change path to xivo projects

--- projects.el
+++ projects.el
@@ -3,16 +3,15 @@
 ;; PATH
 (defconst home-path "/home/pcm")
 (defconst dev-path (concat home-path "/d"))
-(defconst xivo-path (concat dev-path "/x"))
-(defconst xivo-agent-root (concat xivo-path "/agent"))
-(defconst xivo-agid-root (concat xivo-path "/agid"))
-(defconst xivo-ctid-root (concat xivo-path "/ctid"))
-(defconst xivo-dao-root (concat xivo-path "/dao"))
-(defconst xivo-client-root (concat xivo-path "/client-qt"))
+(defconst xivo-agent-root (concat dev-path "/xivo-agent"))
+(defconst xivo-agid-root (concat dev-path "/xivo-agid"))
+(defconst xivo-ctid-root (concat dev-path "/xivo-ctid"))
+(defconst xivo-dao-root (concat dev-path "/xivo-dao"))
+(defconst xivo-client-root (concat dev-path "/xivo-client-qt"))
 (defconst xivo-client-baselib (concat xivo-client-root "/baselib"))
 (defconst xivo-client-xivoclient (concat xivo-client-root "/xivoclient"))
-(defconst xivo-dird-root (concat xivo-path "/dird"))
-(defconst xivo-libpython-root (concat xivo-path "/lib-python"))
+(defconst xivo-dird-root (concat dev-path "/xivo-dird"))
+(defconst xivo-libpython-root (concat dev-path "/xivo-lib-python"))
 (defconst xivo-client-include-path
   (list "/usr/share/qt4/mkspecs/linux-g++-64"
         xivo-client-baselib
