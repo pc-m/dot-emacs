0c54d99200148872b05b28a8f16536826a28a7a2
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2014-12-19 14:49:36 -0500

    fix flymake for xivo-client

--- flymake-clang-c++.el
+++ flymake-clang-c++.el
@@ -1,20 +1,7 @@
 (defun flymake-clang-c++-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (get-clang-command local-file)))
-
-(defun get-clang-command (file)
-  "Returns a list containing the program to run and it's arguments"
-  (message "In get-clang-command")
-  (let* ((a1 '("-fsyntax-only" "-fno-color-diagnostics" "--std=c++11"))
+  (let* ((a1 '("-fsyntax-only" "-fno-color-diagnostics" "--std=c++11" "-fPIC"))
          (a2 (get-project-include-path))
-         (a3 (list local-file)))
-    (message (mapconcat 'identity a1 " "))
-    (message (mapconcat 'identity a2 " "))
-    (message (mapconcat 'identity a3 " "))
+         (a3 (list (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))))
     (let ((args (apply 'append `(,a1 ,a2 ,a3))))
       (list "clang++" args))))
 
--- projects/xivo-client-qt.dirs-local.el
+++ projects/xivo-client-qt.dirs-local.el
@@ -3,13 +3,15 @@
                                                       "/usr/include/x86_64-linux-gnu/qt5/QtCore"
                                                       "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
                                                       "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+                                                      "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
                                                       xivo-client-baselib
                                                       xivo-client-xivoclient
                                                       (concat xivo-client-baselib "/src")
                                                       (concat xivo-client-baselib "/src/storage")
                                                       (concat xivo-client-xivoclient "/src")
                                                       (concat xivo-client-xivoclient "/src/xletlib")
-                                                      (concat xivo-client-xivoclient "/obj"))))))
+                                                      (concat xivo-client-xivoclient "/obj"))))
+         (project-init . xivo-client-qt-init)))
    (c++-mode . ((c-file-style . "xivo")
                 (project-init . xivo-client-qt-init)))
    (c-mode . ((mode . c++)
